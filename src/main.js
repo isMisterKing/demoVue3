4b03a5adef06beb72dd43b0bf9dac9d022a94ba8
wk <1002185190@qq.com>
2024-03-31 23:16:46 +0800

    引入vue-router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import defineGlobalComp from './globalComponents';
-import store from '@/store'
+import store from '@/store';
+import router from '@/router';
 
 const app = createApp(App);
 
 defineGlobalComp(app);
 
-app.use(store)
-
-app.mount('#app');
+app
+  .use(store)
+  .use(router)
+  .mount('#app');
